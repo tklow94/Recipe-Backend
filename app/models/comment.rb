e31891e7ab00bc3822e38978434906e5c35aa012
Trevor <tlow94@uw.edu>
2020-09-15 14:38:24 -0700

    pics

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -2,10 +2,5 @@ class Comment < ApplicationRecord
   belongs_to :commentable, polymorphic: true
   has_many :comments, as: :commentable
   belongs_to :user
-
-  # belongs_to :post
-  # belongs_to :user
-  # belongs_to :parent, class_name: 'Comment', optional: true
-  # has_many :replies, class_name: 'Comment', foreign_key: :parent_id, dependent: :destroy
   
 end
